de8bff5da5cbc0596a5301aecc9ce64ebae833d6
jelly3105 <anjalisherikar3105@gmail.com>
2025-01-06 19:01:38 +0530

    add version1 in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
+import Version1 from './versions/version1';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Version1 />
   </React.StrictMode>
 );
